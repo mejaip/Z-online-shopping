57435c729d697ff0ca151a788ba325a23e4dc35f
Jaipratap Singh <jai123pratap@gmail.com>
2018-01-23 21:45:52 +0530

    Spring Security

--- Z-onlineshopping/src/main/java/net/mejaip/onlineshopping/Handler/RegisterHandler.java
+++ Z-onlineshopping/src/main/java/net/mejaip/onlineshopping/Handler/RegisterHandler.java
@@ -1,6 +1,8 @@
 package net.mejaip.onlineshopping.Handler;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.binding.message.MessageBuilder;
+import org.springframework.binding.message.MessageContext;
 import org.springframework.stereotype.Component;
 
 import net.mejaip.onlineshopping.model.RegisterModel;
@@ -11,51 +13,71 @@ import net.mejaip.shoppingbackend.dto.User;
 
 @Component
 public class RegisterHandler {
-	
+
 	@Autowired
-	private UserDAO userDAO ;
-	
-	public  RegisterModel init()
-	{
+	private UserDAO userDAO;
+
+	public RegisterModel init() {
 		return new RegisterModel();
 	}
-	
-	public void addUser(RegisterModel registerModel, User user)
-	{
+
+	public void addUser(RegisterModel registerModel, User user) {
 		registerModel.setUser(user);
 	}
-	
-	public void addBilling(RegisterModel registerModel, Address billing)
-	{
+
+	public void addBilling(RegisterModel registerModel, Address billing) {
 		registerModel.setBilling(billing);
 	}
-	
-	public String saveAll(RegisterModel model)
-	{
-		String transitionValue ="success";
-		
-		//Fetch the USer
-		User user=model.getUser();
-		
-		if(user.getRole().equals("USER"))
-		{
-			Cart cart= new Cart();
+
+	public String saveAll(RegisterModel model) {
+		String transitionValue = "success";
+
+		// Fetch the USer
+		User user = model.getUser();
+
+		if (user.getRole().equals("USER")) {
+			Cart cart = new Cart();
 			cart.setUser(user);
 			user.setCart(cart);
 		}
-		
+
 		userDAO.addUser(user);
-		
-		//get the billing address
-		
+
+		// get the billing address
+
 		Address billing = model.getBilling();
-		
+
 		billing.setUser(user);
-		
+
 		billing.setBilling(true);
-		
+
 		userDAO.addAddress(billing);
-		
+
+		return transitionValue;
+	}
+
+	public String validateUser(User user, MessageContext error) {
+		String transitionValue = "success";
+		// If password matches  confirm password
+		if (!(user.getPassword().equals(user.getConfirmPassword()))) {
+			error.addMessage(new MessageBuilder()
+					.error()
+					.source("confirmPassword")
+					.defaultText("Password Does not match the confirm Password!")
+					.build()
+					);
+			transitionValue = "failure";
+		}
+		if(userDAO.getByEmail(user.getEmail()) != null)
+		{
+				error.addMessage(new MessageBuilder()
+						.error()
+						.source("email")
+						.defaultText("Email Address Already exist!")
+						.build()
+						);
+			transitionValue = "failure";
+	}
 		return transitionValue;
 	}
 }
